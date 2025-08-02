3450bf970b67280192bc1fb38d2ef0efd612b195
Jancee Orr <jancee31@hotmail.com>
2025-08-02 17:42:10 -0400

    Cleaned up HTML, CSS, and JS layout

--- script/main.js
+++ script/main.js
@@ -1,1 +1,10 @@
 console.log("Welcome to my portfolio site!");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("mobile-menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+});
